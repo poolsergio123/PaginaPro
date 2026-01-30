12eeb7d8a47d8c0d98061c389820d09a9552bec0
poolsergio123 <125148074+poolsergio123@users.noreply.github.com>
2026-01-29 21:56:38 -0500

    Add beforeunload event listener to prevent navigation

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+window.addEventListener("beforeunload", function (e) {
+    e.preventDefault();
+    e.returnValue = "";
+});
+
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("active");}
